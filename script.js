3a1a964ca66a766dafd45cd82cf879516b86846e
Rencho13 <stephone13@gmail.com>
2019-02-23 21:30:17 +0100

    function init et refresh maj

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 window.onload = function () {
-	var canvas;
+
+	var canvasWidth = 900;
+	var canvasHeight = 600;
+
+	var blockSize
+
 	var ctx;
 	var delay = 100;
 	var x = 0;
 	var y = 0;
-	
+
 	init();
-	
-	
+
+
 	function init() {
 
-		canvas = document.createElement('canvas');
-		canvas.width = 900;
-		canvas.height = 600;
+		var canvas = document.createElement('canvas');
+		canvas.width = canvasWidth;
+		canvas.height = canvasHeight;
 		canvas.style.border = "1px solid";
 		document.body.appendChild(canvas);
 		ctx = canvas.getContext("2d");
@@ -20,14 +25,16 @@ window.onload = function () {
 	}
 
 	function refreshCanvas() {
-		x += 2;
-		y += 2;
-		ctx.clearRect(0,0,900,600);
+		x += 10;
+		y += 10;
+		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 		ctx.fillStyle = "#ff0000";
 		ctx.fillRect(x, y, 100, 50);
-		setTimeout(refreshCanvas,delay);		
+		setTimeout(refreshCanvas, delay);
+
 	}
-	
+
+
 
 
 }
