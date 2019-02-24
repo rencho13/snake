b6c2239b1a751466257a1ad6b4fd3112c3d44322
Rencho13 <stephone13@gmail.com>
2019-02-24 20:27:24 +0100

    controle snake au clavier

--- script.js
+++ script.js
@@ -14,44 +14,102 @@ window.onload = function () {
 		var canvas = document.createElement('canvas');
 		canvas.width = canvasWidth;
 		canvas.height = canvasHeight;
-		canvas.style.border = "1px solid";
+		canvas.style.border = '1px solid';
 		document.body.appendChild(canvas);
-		ctx = canvas.getContext("2d");
-		snakee = new Snake([[6,4],[5,4],[4,4]]);
+		ctx = canvas.getContext('2d');
+		snakee = new Snake([[6, 4], [5, 4], [4, 4]]);
 		refreshCanvas();
 	}
 
 	function refreshCanvas() {
-		
+
 		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-		
 		snakee.draw();
+		snakee.advance();
 		setTimeout(refreshCanvas, delay);
 
 	}
-	function drawBlock(ctx, position)
-	{
+
+	function drawBlock(ctx, position) {
 		var x = position[0] * blockSize;
 		var y = position[1] * blockSize;
-		ctx.fillRect(x,y,blockSize,blockSize);
+
+		ctx.fillRect(x, y, blockSize, blockSize);
 	}
-	function Snake(body)
-	{
+
+	function Snake(body, direction) {
 		this.body = body;
-		this.draw = function()
-		{
+		this.direction = direction;
+		this.draw = function () {
 			ctx.save();
 			ctx.fillStyle = "#ff0000";
-			for(var i = 0; i < this.body.length; i++);
-			{
-				drawBlock(ctx,this.body[i]);
+			for (var i = 0; i < this.body.length; i++) {
+				drawBlock(ctx, this.body[i]);
 			}
 			ctx.restore();
-			
 		};
-		
-	}
-	
+		this.advance = function () {
+			var nextPosition = this.body[0].slice();
+			switch (this.direction) {
+				case "left":
+					nextPosition[0] -= 1;
+					break;
+				case "right":
+					nextPosition[0]++;
+					break;
+				case "down":
+					nextPosition[1]++;
+					break;
+				case "up":
+					nextPosition[1]--;
+					break;
+				default:
+					throw ("Invalid Direction");
+			}
+			this.body.unshift(nextPosition);
+			this.body.pop();
+		};
+		this.setDirection = function (newDirection) {
+			var allowedDirections;
+			switch (this.direction) {
+				case "left":
+				case "right":
+					allowedDirections = ["up", "down"];
+					break;
+				case "down":
+				case "up":
+					allowedDirections = ["left", "right"];
+					break;
+				default:
+					throw ("Invalid Direction")
+			}
+			if (allowedDirections.indexOf(newDirection) > -1) {
+				this.direction = newDirection;
+			}
 
+		};
+	}
+
+	document.onkeydown = function handleKeyDown(e) {
+		var key = e.keyCode;
+		var newDirection;
+		switch (key) {
+			case 37: //touche gauche
+				newDirection = "left";
+				break;
+			case 38: // touche en haut
+				newDirection = "up";
+				break;
+			case 39: // touche droite
+				newDirection = "right";
+				break;
+			case 40: // touche en bas
+				newDirection = "down";
+				break;
+			default:
+				return;
+		}
+		snakee.setDirection(newDirection);
+	}
 
 }
